c858137e670662934997ff61378d1127f94aead3
Daniël ten Wolde <35001806+Dtenwolde@users.noreply.github.com>
2020-08-11 13:04:25 +0200

    Added race selection on character creation

--- services/client/src/components/home/CharacterCreation.js
+++ services/client/src/components/home/CharacterCreation.js
@@ -1,29 +1,66 @@
 import React from "react";
-import Dropdown from 'react-dropdown';
+import {makeStyles} from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import {characterCreationService} from "../services/characterCreationService";
+import ListSubheader from "@material-ui/core/ListSubheader";
 
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
 
 export default function CharacterCreation(props) {
     const user = props.user
     const [races, setRaces] = React.useState([])
+
+    const classes = useStyles();
+    const [race, setRace] = React.useState('');
+
+    const handleChange = (event) => {
+        console.log(event.target.value);
+        setRace(event.target.value);
+    };
+
+    React.useEffect(() => {
+        getRaces()
+    }, []);
 
     function getRaces() {
         characterCreationService.getRaces().then(r => {
             setRaces(r);
         })
     }
-    console.log('got here')
-    return <div className={"main-content"}>
-        <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Dropdown Button
-            </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
+    console.log(races)
+
+    return <div className={"main-content"}>
+        <FormControl className={classes.formControl}>
+            <InputLabel id="race-simple-select-label">Race</InputLabel>
+            <Select
+                labelId="race-select-label"
+                id="race-select"
+                value={race}
+                onChange={handleChange}
+            >
+                {
+                    races.map((race, i) => {
+                        return <MenuItem key={i} value={race.name}>
+                            {race.name}
+                        </MenuItem>;
+
+                    })
+                }
+            </Select>
+        </FormControl>
     </div>
 }
